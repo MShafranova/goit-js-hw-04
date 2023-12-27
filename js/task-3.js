c7c12cd7b946117cfaee4bd9dbfd97784d30f0a2
MShafranova <mariia.shafranova@gmail.com>
2023-12-27 22:43:00 +0100

    task-3 done

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,14 @@
 const profile = {
-    username: "Jacob",
+  username: "Jacob",
   playTime: 300,
+  changeUsername(newName) {
+    this.username = newName;
+  },
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
+  getInfo() {
+    return `${profile.username} has ${profile.playTime} active hours!`;
+  },
 };
-changeUsername(newName){}
+
