15734dc40072d5a400031ec78bfacf13412872e2
MShafranova <mariia.shafranova@gmail.com>
2023-12-28 08:56:13 +0100

    task-3 fix

--- js/task-2.js
+++ js/task-2.js
@@ -1,10 +1,10 @@
 function calcAverageCalories(days) {
   let totalCalories = 0;
-  for (const day of days) {
-    totalCalories += day.calories;
-  }
   if (days.length === 0) {
     return 0;
+  }
+  for (const day of days) {
+    totalCalories += day.calories;
   }
   return totalCalories / days.length;
 }
